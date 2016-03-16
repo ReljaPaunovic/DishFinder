a236058e6d4cb95504ed170e2d970a322481ac97
WYHNUS <wangyanhao1993@163.com>
2016-03-16 16:02:58 +0100

    save changes for merge

--- Website/finderApp/urls.py
+++ Website/finderApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^recipe/([0-9]+)/$', views.recipe, name='recipe'),
+	url(r'^recipe/(?P<recipe_id>[0-9]+)/$', views.recipe, name='recipe'),
 	url(r'^search/$', views.search_result, name='search result'),
 ]
