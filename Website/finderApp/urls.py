31f5b6b4233e1f59531179cb0c3c75b052174bcd
WYHNUS <wangyanhao1993@163.com>
2016-03-27 23:50:30 +0200

    add signup page

--- Website/finderApp/urls.py
+++ Website/finderApp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^recipe/(?P<recipe_id>[0-9]+)/$', views.recipe, name='recipe'),
 	url(r'^search/$', views.search_result, name='search result'),
+	url(r'^signup/$', views.signup, name='signup'),
 ]
--- Website/finderApp/views.py
+++ Website/finderApp/views.py
@@ -3,10 +3,27 @@ from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
 from .models import Ingredient
 from .models import Recipe
 
 import json
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/books/")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {
+        'form': form,
+    })
 
 def index(request):
 	ingredient_list = Ingredient.objects.all()
@@ -15,6 +32,16 @@ def index(request):
 		'ingredient_list': ingredient_list,
 	}
 	return HttpResponse(template.render(context, request))
+
+def signup(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			new_user = form.save()
+			return HttpResponseRedirect("/finderApp/")
+	else:
+		form = UserCreationForm()
+	return render(request, 'finderApp/signup.html', {'form': form})
 
 def search_result(request):
 	template = loader.get_template('finderApp/search_result.html')
