ff55d49e769dd332c330fe27d4efef99f7796614
WYHNUS <wangyanhao1993@163.com>
2016-04-03 16:28:04 +0200

    add Category model and update bootstrap data

--- Website/finderApp/models.py
+++ Website/finderApp/models.py
@@ -4,6 +4,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Category model
+class Category(models.Model):
+	name = models.CharField(max_length=100, default="no name")
+
+	def __str__(self):
+		return self.name
+
+# Recipe related models
 class Ingredient(models.Model):
 	ingredient_name = models.CharField(max_length=200)
 
@@ -20,9 +29,9 @@ class Recipe(models.Model):
 	name = models.CharField(max_length=200, default="no name")
 	image = models.URLField(max_length=200, default="")
 	servings = models.PositiveSmallIntegerField(default=2)
-	category = models.PositiveSmallIntegerField(default=2)
 	contained_ingredients = models.ManyToManyField(Ingredient)
 	directions = models.ManyToManyField(Direction)
+	category = models.ForeignKey(Category, null=True, blank=True, default=None)
 	creater = models.ForeignKey(User, null=True, blank=True, default=None)
 
 	def __str__(self):
